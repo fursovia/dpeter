4ecbc8395e1e332341472d80f5a379dc46aa21ae
fursovia <fursov.ia@gmail.com>
2020-10-22 03:07:46 +0300

    calc embeddings from length

--- dpeter/models/img2sentence.py
+++ dpeter/models/img2sentence.py
@@ -19,7 +19,6 @@ class Img2Sentence(Model):
 
     MAX_SENTENCE_LENGTH = 128
     NUM_CHANNELS = 288
-    PADDING_ID = 0
 
     def __init__(
             self,
@@ -35,6 +34,10 @@ class Img2Sentence(Model):
         self._emb_dim = emb_dim
         self._att_dim = att_dim
         self._input_dim = input_dim
+        self._embedder = torch.nn.Embedding(
+            num_embeddings=self.vocab.get_vocab_size("tokens"),
+            embedding_dim=self._emb_dim  # TODO
+        )
         # we have 71 max sentence length
         self._positional_embedder = torch.nn.Embedding(
             num_embeddings=self.MAX_SENTENCE_LENGTH,
@@ -55,8 +58,12 @@ class Img2Sentence(Model):
         self._wo = torch.nn.Linear(self._seq2seq_encoder.get_output_dim(), self.vocab.get_vocab_size("tokens"))
         self._wu2 = torch.nn.Linear(self.NUM_CHANNELS, self.vocab.get_vocab_size("tokens"))
 
-        ignore_index = self.vocab.get_token_index(DEFAULT_PADDING_TOKEN)
-        self._loss = torch.nn.CrossEntropyLoss(ignore_index=ignore_index)
+        self._padding_index = self.vocab.get_token_index(DEFAULT_PADDING_TOKEN)
+        self._start_index = self.vocab.get_token_index(START_TOKEN)
+        self._end_index = self.vocab.get_token_index(END_TOKEN)
+        self._unk_index = self.vocab.get_token_index(DEFAULT_OOV_TOKEN)
+
+        self._loss = torch.nn.CrossEntropyLoss(ignore_index=self._padding_index)
 
     def _get_mask_from_length(self, length: torch.Tensor) -> torch.Tensor:
         length_numpy = length.cpu().numpy()
@@ -69,7 +76,14 @@ class Img2Sentence(Model):
         return torch.tensor(mask, dtype=torch.bool, device=length.device)
 
     def _get_embeddings_from_length(self, length: torch.Tensor) -> torch.Tensor:
-        pass
+        max_length = length.max().item() + 2  # we also add start/end tokens
+        token_ids = torch.full(size=(length.size(0), max_length), fill_value=self._padding_index)
+        token_ids[:, 0] = self._start_index
+        token_ids[:, length + 1] = self._end_index
+        # TODO: UNK tokens
+
+        embeddings = self._embedder(token_ids)
+        return embeddings
 
     def forward(
             self,
@@ -90,32 +104,37 @@ class Img2Sentence(Model):
         # (batch_size, 125, 13, 288)
         features = features.transpose(1, 3)
 
-        if text is not None and length is not None:
+        if length is not None:
             mask = get_text_field_mask(text)
         else:
             with torch.no_grad():
                 length = self._length_classifier(image)
                 length = torch.clamp_min(length, 1).type(torch.long)
+                length = length.to(image.device)
 
             mask = self._get_mask_from_length(length)
 
-        max_length = length.max().item()
+        max_length = length.max().item() + 2
         position_ids = torch.arange(max_length).unsqueeze(0).expand([batch_size, max_length])
         positional_embeddigns = self._positional_embedding(position_ids)
 
-        # TODO: do it right
         # START/END/UNK/PADDING embeddings
-        embeddings = torch.rand(batch_size, max_length, self._emb_dim)
+        embeddings = self._get_embeddings_from_length(length)
 
         embeddings = positional_embeddigns + embeddings
 
         # (batch_size, num_tokens, 1625)
         attention_weights = self._attention(embeddings, features)
+        # (batch_size, num_tokens, NUM_CHANNELS)
         attention_embeddings = self._attention.get_attention_features(features, attention_weights)
 
+        # (batch_size, input_dim)
         embeddings = self._wc(embeddings) + self._wu1(attention_embeddings)
 
+        # (batch_size, hidden_dim)
         contextual_embeddings = self._seq2seq_encoder(embeddings, mask=mask)
+
+        # (batch_size, vocab_size)
         logits = self._wo(contextual_embeddings) + self._wu2(attention_embeddings)
 
         output_dict = {"logits": logits}
@@ -125,5 +144,9 @@ class Img2Sentence(Model):
                 logits.transpose(1, 2),
                 text["tokens"]["tokens"],
             )
+
+        if not self.training:
+            # TODO: calculate CER, WER, Accuracy
+            pass
 
         return output_dict
