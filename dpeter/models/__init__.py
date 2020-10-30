e7cd100b0cc22d470a67f65731ef3f04412174e7
fursovia <fursov.ia@gmail.com>
2020-10-30 22:11:52 +0300

    disabled imports

--- dpeter/models/__init__.py
+++ dpeter/models/__init__.py
@@ -1,3 +1,3 @@
-from .length_classifier import LengthClassifier
-from .img2sentence import Img2Sentence
-from .generative_img2sent import GenerativeImg2Sentence
+# from .length_classifier import LengthClassifier
+# from .img2sentence import Img2Sentence
+# from .generative_img2sent import GenerativeImg2Sentence
