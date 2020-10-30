857edb49a50ae5167dffb80b7078b2812038fb32
fursovia <fursov.ia@gmail.com>
2020-10-30 21:23:08 +0300

    added predict file

--- dpeter/constants.py
+++ dpeter/constants.py
@@ -6,3 +6,6 @@ NUM_CHANNELS = 3
 WHITE_CONSTANT = 255
 VALUES_ABOVE_THIS_ARE_ALSO_WHITE = 230
 PROJECT_NAME = "digital_peter"
+CHARSET = " оаетнисрлвдкмпубiѣяычгзъжхшйюфц1ь+щ[0]27345э8erps96tfіhcn⊕m⊗daglb)–|×o/kuǂ…"
+MAX_LENGTH = 80
+INPUT_SIZE = (WIDTH, HEIGHT, 1)
