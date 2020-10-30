6e3a35e38d2cd7c64eacbb8a3dbc6b14d79656b9
fursovia <fursov.ia@gmail.com>
2020-10-30 22:17:05 +0300

    added bs

--- dpeter/commands/tf_predict.py
+++ dpeter/commands/tf_predict.py
@@ -61,7 +61,7 @@ def main(serialization_dir: Path, data_dir: Optional[Path] = None, out_path: Opt
         names.append(image_path.stem)
 
     images = normalization(images)
-    predicts, probabilities = model.predict(images, ctc_decode=True)
+    predicts, probabilities = model.predict(images, batch_size=BATCH_SIZE, ctc_decode=True)
     predicts = [tokenizer.decode(x[0]) for x in predicts]
 
     out_path.mkdir(exist_ok=True, parents=True)
