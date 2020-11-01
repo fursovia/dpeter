c866d353f27bfab1d2af2819a9d5a3ff5d6252b6
Ivan Fursov <fursovia@gmail.com>
2020-11-01 17:45:37 +0000

    deslant all files

---
+++ bin/deslant.sh
@@ -0,0 +1,13 @@
+
+
+DATA_DIR="data/images"
+OUT_DIR="data/deslant_images"
+
+mkdir -p ${OUT_DIR}
+
+
+for img in ${DATA_DIR}/*.jpg; do
+	img_name=$(basename ${img})
+	echo "deslanting ${img_name}"
+	/home/DeslantImg/DeslantImg ${img} ${OUT_DIR}/${img_name}
+done
