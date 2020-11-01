df51354e63392bae8469fb681d1d3dff5764d09b
fursovia <fursov.ia@gmail.com>
2020-11-02 01:12:38 +0300

    increased beam

--- dpeter/commands/tf_predict.py
+++ dpeter/commands/tf_predict.py
@@ -1,8 +1,7 @@
-from typing import Optional, List
+from typing import Optional
 from pathlib import Path
 
 import typer
-import editdistance
 import numpy as np
 
 from dpeter.constants import CHARSET, MAX_LENGTH, INPUT_SIZE
@@ -13,20 +12,9 @@ from dpeter.models.htr_model import HTRModel
 app = typer.Typer()
 
 
-NUM_SAMPLES = 50
 ARCH = "flor"
 BATCH_SIZE = 16
-NUM_EPOCHS = 100
-
-
-def find_indexes_of_the_worst_predicitons(y_pred: List[str], y_true: List[str], k: int = NUM_SAMPLES) -> np.ndarray:
-
-    dists = []
-    for yt, yp in zip(y_true, y_pred):
-        dists.append(editdistance.eval(yt, yp))
-    dists = np.array(dists)
-    indexes = dists.argsort()[::-1][:k]
-    return indexes
+BEAM_SIZE = 100
 
 
 @app.command()
@@ -44,9 +32,7 @@ def main(serialization_dir: Path, data_dir: Optional[Path] = None, out_path: Opt
         architecture=ARCH,
         input_size=INPUT_SIZE,
         vocab_size=len(CHARSET) + 2,
-        beam_width=10,
-        stop_tolerance=20,
-        reduce_tolerance=15,
+        beam_width=BEAM_SIZE,
         top_paths=1
     )
 
