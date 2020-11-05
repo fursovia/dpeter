e881e85065df8e46adddfeda7032935767876656
fursovia <fursov.ia@gmail.com>
2020-11-05 23:53:14 +0300

    dont use /root

--- dpeter/utils/postprocessors/seq2seq.py
+++ dpeter/utils/postprocessors/seq2seq.py
@@ -1,5 +1,12 @@
+import json
+from pathlib import Path
+import os
+import shutil
+
 from allennlp_models.generation.predictors import Seq2SeqPredictor
-from allennlp.models.archival import load_archive
+from allennlp.models.archival import _load_dataset_readers, _load_model, get_weights_path, Archive, load_archive, extracted_archive
+from allennlp.common import Params
+from allennlp.common.file_utils import cached_path
 
 from dpeter.utils.postprocessors.postprocessor import Postprocessor
 
@@ -8,8 +15,41 @@ from dpeter.utils.postprocessors.postprocessor import Postprocessor
 class Seq2seqPostprocessor(Postprocessor):
 
     # TODO: add batches
-    def __init__(self, archive_path: str, beam_size: int = 10) -> None:
-        archive = load_archive(archive_path, overrides={'model': {'beam_size': beam_size}})
+    def __init__(self, archive_path: str, beam_size: int = 10, cuda_device: int = 0) -> None:
+
+        resolved_archive_path = cached_path(archive_path, cache_dir="presets")
+
+        tempdir = None
+        try:
+            if os.path.isdir(resolved_archive_path):
+                serialization_dir = resolved_archive_path
+            else:
+                with extracted_archive(resolved_archive_path, cleanup=False) as tempdir:
+                    serialization_dir = tempdir
+
+            weights_path = get_weights_path(serialization_dir)
+
+            # Load config
+            params = json.load(open(str(Path(serialization_dir) / "config.json")))
+            params["model"]["beam_size"] = beam_size
+            config = Params(params=params)
+
+            # Instantiate model and dataset readers. Use a duplicate of the config, as it will get consumed.
+            dataset_reader, validation_dataset_reader = _load_dataset_readers(
+                config.duplicate(), serialization_dir
+            )
+            model = _load_model(config.duplicate(), weights_path, serialization_dir, cuda_device)
+        finally:
+            if tempdir is not None:
+                shutil.rmtree(tempdir, ignore_errors=True)
+
+        archive = Archive(
+            model=model,
+            config=config,
+            dataset_reader=dataset_reader,
+            validation_dataset_reader=validation_dataset_reader,
+        )
+
         self._predictor = Seq2SeqPredictor.from_archive(archive)
 
     def postprocess(self, text: str) -> str:
